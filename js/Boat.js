efb5cc89ee9fa8035c0bf9eab9b7eb0f28b2a1c3
arpitchaudhary <92873915+arpit9460216600@users.noreply.github.com>
2022-02-21 20:02:50 +0530

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,33 @@
+class Boat {
+
+  constructor(x, y, width, height,boatPos) {
+    var options = {
+      isStatic: true
+    };
+    this.width = width;
+    this.height = height;
+    this.boatPosition=boatPos
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.image=loadImage("./assets/boat.png");
+    World.add(world, this.body);
+  }
+  display() {
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    fill("brown");
+    imageMode(CENTER);
+    image(this.image,0,this.boatPosition, this.width, this.height);
+    noTint()
+    
+    pop();
+  }
+}
+
+
+
+
+
+
+
+
